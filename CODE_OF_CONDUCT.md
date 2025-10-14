bad7a5b60b346f40fd32ac0d532f9167d4cef2da
okwhynot <153409242+okwhynot-wtf@users.noreply.github.com>
2025-10-14 16:24:11 +1100

    prep second version

--- CODE_OF_CONDUCT.md
+++
@@ -1,3 +0,0 @@
-# Code of Conduct
-
-Be respectful and constructive. Harassment or discrimination is not tolerated.
--- OnlyOne/Basic.lean
+++
@@ -1,4 +0,0 @@
--- Optional placeholder module; safe to ignore or extend.
-namespace OnlyOneBasics
-def hello := "OnlyOne basics loaded"
-end OnlyOneBasics
